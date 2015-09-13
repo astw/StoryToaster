77d36d223e2f5e625d8de8211192fec21165bdb5
shuhao wang <wshuhao@gmail.com>
2015-09-13 15:20:09 -0700

    UI

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -124,7 +124,7 @@
       imageUrl = "http://localhost:3000" + imageUrl;
       fabric.Image.fromURL(imageUrl, function(img) {
         console.log(vm.left_canvas == true);
-        if(vm.leftActive == true)
+        if(vm.currentPage.left.active == true)
            vm.left_canvas.add(img);
         else
            vm.right_canvas.add(img);
@@ -147,102 +147,53 @@
     };
 
     vm.previewClick = function(page,which) {
-      currentPageNum = page.pageNumber;
-
-      vm.currentPage = page;
+       if(vm.currentPage != page) {
+        backCurrentDesignData();
+        vm.currentPage = page;
+        vm.left_canvas.clear();
+        vm.right_canvas.clear();
+        restoreToCurrentDesignData()
+      }
 
       for (var i = 0; i < vm.pages.length; i++) {
         vm.pages[i].left.active = false;
         vm.pages[i].right.active = false;
       }
-      if(which === 'left'){
+      if (which === 'left') {
         page.left.active = true;
       }
       else {
-        page.right.active = true; 
+        page.right.active = true;
       }
     };
 
     vm.nextPage = function(){
-      // save the current page
-      var page ={
-        left:{
-          data:JSON.stringify(vm.left_canvas)
-        },
-        right:{
-          data:JSON.stringify(vm.right_canvas)
-        }
-      };
-      //
-      //var page ={
-      //  left:{
-      //    data:JSON.stringify(vm.left_canvas.toSVG())
-      //  },
-      //  right:{
-      //    data:JSON.stringify(vm.right_canvas.toSVG())
-      //  }
-      //};
-
-      //var page ={
-      //  left:{
-      //    data:JSON.stringify(vm.left_canvas.toDatalessJSON())
-      //  },
-      //  right:{
-      //    data:JSON.stringify(vm.right_canvas.toDatalessJSON())
-      //  }
-      //};
-
-      vm.pages.push(page);
-
+      backCurrentDesignData();
       vm.left_canvas.clear();
       vm.right_canvas.clear();
-
-      //var objs = vm.left_canvas.getObjects();
-      //
-      //objs.forEach(function(obj){
-      //  vm.left_canvas.remove(obj);
-      //});
-      //
-      //objs = vm.right_canvas.getObjects();
-      //objs.forEach(function(obj){
-      //  vm.right_canvas.remove(obj);
-      //});
-
     };
 
     vm.previousPage = function(currentIndex){
-      var pageLeft = vm.pages[0].left.data;
-      var pageRight = vm.pages[0].right.data;
+      backCurrentDesignData();
+    };
 
-      //vm.left_canvas.loadFromDatalessJSON(pageLeft);
-      //vm.right_canvas.loadFromDatalessJSON(pageRight);
+    var backCurrentDesignData = function(){
+      vm.currentPage.left.imageData = JSON.stringify(vm.left_canvas);//   vm.left_canvas.toDataURL();
+      vm.currentPage.right.imageData =JSON.stringify(vm.right_canvas) ;// vm.right_canvas.toDataURL();
+    };
 
-      vm.left_canvas.renderAll();
-      vm.right_canvas.renderAll();
-      //vm.left_canvas.renderAll().bind(vm.left_canvas);
-      //vm.right_canvas.renderAll().bind(vm.right_canvas);
+    var restoreToCurrentDesignData = function(){
+      var leftData = vm.currentPage.left.imageData;
+      var rightData =vm.currentPage.right.imageData;
 
-      //fabric.loadSVGFromString(pageLeft,function(objects,options){
-      //  var obj = fabric.util.groupSVGElements(objects,options);
-      //  vm.left_canvas.add(obj).renderAll();
-      //});
-      //
-      //
-      //fabric.loadSVGFromString(pageRight,function(objects,options){
-      //  var obj = fabric.util.groupSVGElements(objects,options);
-      //  vm.right_canvas.add(obj).renderAll();
-      //});
-      //
-      vm.left_canvas.loadFromJSON(pageLeft,vm.left_canvas.renderAll.bind(vm.left_canvas),function(){});
-      vm.right_canvas.loadFromJSON(pageRight,vm.right_canvas.renderAll.bind(vm.right_canvas),function(){});
-      //
-      //vm.left_canvas.renderAll();
-      //vm.right_canvas.renderAll();
+      if(leftData)
+      vm.left_canvas.loadFromJSON(leftData,vm.left_canvas.renderAll.bind(vm.left_canvas),function(){});
+      if(rightData)
+      vm.right_canvas.loadFromJSON(rightData,vm.right_canvas.renderAll.bind(vm.right_canvas),function(){});
     };
 
     vm.nextSaveToImage = function() {
 
-      $scope.preview_image = vm.left_canvas.toDataURL();
       //var preview_image = $document.find('#preview_image');
       //var preview_image =  $document.find('preview_image');  /// $('#preview_image');
       var preview_image = $(event.target).find('#preview_image');
