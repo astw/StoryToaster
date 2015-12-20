8d184f8d292ab68e342e637ed3134bb2d285f219
wshuhao <wshuhao@gmail.com>
2015-12-20 15:12:00 -0800

    ui

--- src/app/layout/header/header-controller.js
+++ src/app/layout/header/header-controller.js
@@ -5,18 +5,56 @@
     .module('storyToaster')
     .controller('HeaderController', HeaderController);
 
-
   /* @ngInject */
-  function HeaderController(dependency) {
+  function HeaderController($log,
+                            authService,
+                            $state,
+                            $sessionStorage,
+                            $modal,
+                            $window) {
     var vm = this;
-    vm.title = 'HeaderController';
+
+    vm.storage = $sessionStorage;
+    vm.localeService = localeService;
+    vm.isLoggedIn = authService.isAuthenticated;
+
+    vm.openLogin = openLogin;
+    vm.logOut = logOut;
 
     activate();
 
-    ////////////////
+    //////////////////////////////////
 
+    /**
+     * Controller startup function
+     */
     function activate() {
-    }
+    }//end activate()
+
+
+    /**
+     * Open modal dialog
+     */
+    function openLogin(){
+
+      var modalInstance = $modal.open({
+        templateUrl: 'app/account/login.html',
+        controller: 'AccountController',
+        controllerAs: 'login'
+      });
+
+    }//end openLogin()
+
+
+
+    /**
+     * Log Out user
+     */
+    function logOut(){
+      $log.debug('header.logOut()');
+      authService.logOut();
+      $state.go('app.home');
+    }//end logOut()
   }
 
 })();
