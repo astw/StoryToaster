cdf2f3329bce9b16ab3bd548b61f27d63d3a7ad4
wshuhao <wshuhao@gmail.com>
2016-01-29 23:07:51 -0800

    add from json

--- src/app/fabric/ImageTextbox.js
+++ src/app/fabric/ImageTextbox.js
@@ -1,48 +1,58 @@
+(function(global) {
 
-fabric.ImageTextbox = fabric.util.createClass(fabric.IText, {
+  "use strict";
+
+  fabric.ImageTextbox = fabric.util.createClass(fabric.IText, {
 
 //var ImageTextbox = fabric.util.createClass(fabric.Textbox, {
 ///http://stackoverflow.com/questions/10009080/interactive-text-fields-with-fabric-js
 
+    type: 'imageTextbox',
+    src: '',
 
-  type: 'imageTextbox',
-  src:'',
+    initialize: function (text, options) {
+      options || (options = {});
 
-  initialize: function(text,options) {
-    options || (options = { });
+      this.left = 20;
+      this.top = 20;
 
-    this.left = 20;
-    this.top = 20;
+      this.callSuper('initialize', text);
+      this.fontSize = options.fontSize;
 
-    this.callSuper('initialize', text);
-    this.fontSize = options.fontSize;
+      this.image = new Image();
+      this.image.crossOrigin = "Anonymous";
+      this.image.src = options.src;
 
-    this.image = new Image();
-    this.image.crossOrigin = "Anonymous";
-    this.image.src = options.src;
+      this.image.onload = (function () {
+        this.loaded = true;
+        this.setCoords();
+        this.fire('image:loaded')
+        this.canvas.renderAll();
+      }).bind(this);
+    },
 
-    this.image.onload = (function(){
-      this.loaded = true;
-      this.setCoords();
-      this.fire('image:loaded')
-      this.canvas.renderAll();
-    }).bind(this);
-  },
+    toObject: function () {
+      return fabric.util.object.extend(this.callSuper('toObject'), {
+        label: this.get('label'),
+        src: this.image.src
+      });
+    },
 
-  toObject: function() {
-    return fabric.util.object.extend(this.callSuper('toObject'), {
-      label: this.get('label')
-    });
-  },
+    _render: function (ctx) {
+      console.log('rendering imageTextBox', this.left);
+      if (this.loaded) {
+        ctx.drawImage(this.image, -this.width / 2 - 20, -this.height / 2 - 10, this.width + 40, this.height + 40);
+      }
 
-  _render: function(ctx) {
-    console.log('rendering imageTextBox', this.left);
-    if(this.loaded)
-    {
-      ctx.drawImage(this.image, -this.width/2 - 20, -this.height /2  -10 , this.width + 40, this.height + 40 );
+      this.callSuper('_render', ctx);
     }
+  });
 
-    this.callSuper('_render', ctx);
-  }
-});
+  fabric.ImageTextbox.fromObject = function (object) {
+    var instance = new fabric.ImageTextbox(object.text, fabric.util.object.clone(object), function () {
+      return instance && instance.canvas && instance.canvas.renderAll();
+    });
+    return instance;
+  };
 
+})( typeof exports != 'undefined' ? exports : this);
